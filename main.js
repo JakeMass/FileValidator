e4b025c491a1fe968e1830ca8cf09ddd2d91b27c
Jakob Maßem <jakob.massem@gmx.de>
2022-06-21 19:15:34 +0200

    init

---
+++ main.js
@@ -0,0 +1,51 @@
+import mapping from './filetypes.json' assert { type: "json" };
+
+const input = document.querySelector('#input');
+
+input.addEventListener('change', () => {
+    const file = input.files[0];
+
+    const reader = new FileReader();
+
+    const headerSize = 30;
+
+    reader.onload = () => {
+        const values = new Uint8Array(reader.result);
+        let mimeType = '';
+
+        let header = [];
+
+        for (let i = 0; i < headerSize; i++) {
+            let hex = values[i].toString(16).toUpperCase();
+
+            if (hex.length < 2) {
+                hex = `0${hex}`;
+            }
+
+            header.push(hex);
+        }
+
+        Object.keys(mapping).forEach(key => {
+            const type = mapping[key];
+
+            const hexcode = type.hexcode.split(' ');
+
+            let found = true;
+
+            hexcode.forEach((byte, index) => {
+                if (byte !== header[index + type.offset]) {
+                    found = false;
+                }
+            });
+
+            if (found) {
+                mimeType = type.mime;
+            }
+        });
+
+        console.log(mimeType);
+    };
+
+    reader.readAsArrayBuffer(file);
+});
+
